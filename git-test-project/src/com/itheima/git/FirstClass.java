2be041c1c0763d180c85859b012a5ed6ce818331
Hourn <2513643706@qq.com>
2024-04-01 21:58:12 +0800

    测试IDEA提交

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * @Author: 25136
+ * @Description:
+ * @Date: 2024/4/1 21:48
+ * @Version: 1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
