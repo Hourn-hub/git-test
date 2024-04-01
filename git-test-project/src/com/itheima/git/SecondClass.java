f24939cdf78a435cab6a4d6ba1bb32c85ce2ab1e
Hourn <2513643706@qq.com>
2024-04-01 22:19:57 +0800

    修改

--- git-test-project/src/com/itheima/git/SecondClass.java
+++ git-test-project/src/com/itheima/git/SecondClass.java
@@ -3,12 +3,11 @@ package com.itheima.git;
 /**
  * @Author: 25136
  * @Description:
- * @Date: 2024/4/1 21:48
+ * @Date: 2024/4/1 22:19
  * @Version: 1.0
  */
-public class FirstClass {
+public class SecondClass {
     public static void main(String[] args) {
-        System.out.println("git测试");
-        System.out.println("test2修改");
+        System.out.println("test");
     }
 }
